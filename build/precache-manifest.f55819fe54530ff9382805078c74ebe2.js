6702dd2e96cbcb46ecd08318530e7d5eabd7422a
Naik1994 <42242055+Naik1994@users.noreply.github.com>
2019-03-17 14:15:19 +0530

    Add files via upload

---
+++ build/precache-manifest.f55819fe54530ff9382805078c74ebe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "466c171628ca59c2f9ef",
+    "url": "/static/js/main.7a002904.chunk.js"
+  },
+  {
+    "revision": "01138a573b34bfe6f10c",
+    "url": "/static/js/2.85996f6a.chunk.js"
+  },
+  {
+    "revision": "466c171628ca59c2f9ef",
+    "url": "/static/css/main.7860764e.chunk.css"
+  },
+  {
+    "revision": "01138a573b34bfe6f10c",
+    "url": "/static/css/2.19703e7c.chunk.css"
+  },
+  {
+    "revision": "17bb32a5bad23bbcfe4e915f4ecfb6b8",
+    "url": "/index.html"
+  }
+];
